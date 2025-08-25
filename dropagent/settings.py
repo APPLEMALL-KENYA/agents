15f0549934346d1e5966415ca760d0b5b0ce46dd
applemall1 <appleonlinemall33@gmail.com>
2025-08-25 17:31:37 +0300

    Prepare for Vercel deployment

--- dropagent/settings.py
+++ dropagent/settings.py
@@ -117,3 +117,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.vercel.app",
+    "https://*.now.sh",
+    "https://agents-production-3109.up.railway.app",
+]
